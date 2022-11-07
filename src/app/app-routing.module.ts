982023fb84ef2868db4ecb8caf9953d86344cb34
Igor Iwaniak <igornocny@gmail.com>
2022-11-07 20:20:33 +0100

    feat: categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 import { ProductListComponentModule } from './components/product-list/product-list.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
+import { CategoriesComponentModule } from './components/categories/categories.component-module';
+import { CategoryServiceModule } from './services/category.service-module';
 
 @NgModule({
-  imports: [RouterModule.forRoot([{ path: 'products', component: ProductListComponent }]), ProductListComponentModule, ProductsServiceModule],
+  imports: [RouterModule.forRoot([{
+    path: 'products', component: ProductListComponent }, {
+    path: 'categories', component: CategoriesComponent }]), ProductListComponentModule,
+    ProductsServiceModule, CategoriesComponentModule, CategoryServiceModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/categories/categories.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { Observable } from "rxjs";
+import { ProductModel } from "../../models/product.model";
+import { CategoryService } from "../../services/category.service";
+
+@Component({
+  selector: 'app-categories',
+  templateUrl: './categories.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CategoriesComponent {
+  readonly list$: Observable<ProductModel[]> = this._categoriesService.getAll();
+
+  constructor(private _categoriesService: CategoryService) {
+  }
+}
+
