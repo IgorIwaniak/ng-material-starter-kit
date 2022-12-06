779d40ecdbaee7b20a72e440cc9f89e9cb92c7e1
Igor Iwaniak <igornocny@gmail.com>
2022-12-06 12:44:13 +0100

    feat: filtered-product-list on subject Advanced list

--- src/app/components/filtered-product-list/filtered-product-list.component.ts
+++ src/app/components/filtered-product-list/filtered-product-list.component.ts
@@ -1,11 +1,9 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import {ActivatedRoute, Params} from '@angular/router';
-import { combineLatest, Observable, } from 'rxjs';
+import { Observable, Subject, combineLatest } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
 import { ProductModel } from '../../models/product.model';
 import { CategoryService } from '../../services/category.service';
 import { ProductsService } from '../../services/products.service';
-import {map} from "rxjs/operators";
-
 
 @Component({
   selector: 'app-filtered-product-list',
@@ -15,13 +13,19 @@ import {map} from "rxjs/operators";
 })
 export class FilteredProductListComponent {
   readonly categories$: Observable<string[]> = this._categoryService.getAll();
+  private _categorySubject: Subject<string> = new Subject<string>();
+  public category$: Observable<string> = this._categorySubject.asObservable();
   readonly products$: Observable<ProductModel[]> = combineLatest([
-  this._productsService.getAll(),
-  this._activatedRoute.params
-    ]).pipe(map(([products, params]: [ProductModel[], Params]) => {
-return products.filter((product: ProductModel) => product.category === params['category']);
-  }));
+    this._productsService.getAll(),
+    this.category$
+  ]).pipe(map(([products, category]: [ProductModel[], string]) => {
+    return products.filter(product => product.category === category);
+  })
+  );
+  constructor(private _categoryService: CategoryService, private _productsService: ProductsService) {
+  }
 
-  constructor(private _categoryService: CategoryService, private _productsService: ProductsService, private _activatedRoute: ActivatedRoute) {
+  selectCategory(category: string): void {
+    this._categorySubject.next(category);
   }
 }
